a26bee0467cbef82b6e6133fd2b773c36669844f
Satapitchakorn Punjarat <satapitchakorn.punjarat@allianz.com>
2020-08-04 10:15:03 +0700

    Refactor code.

--- src/main/java/CircularBuffer.java
+++ src/main/java/CircularBuffer.java
@@ -1,47 +1,34 @@
 public class CircularBuffer {
 
-    private int bufferSize = 10;
-    private String[] buffer = new String[bufferSize];
-    private int readPointer = 0;
-    private int writePointer = 0;
+    private final int bufferSize;
+    private final String[] buffer;
+    private int readPointer;
+    private int writePointer;
+
+    public CircularBuffer() {
+        this(10);
+    }
+
+    public CircularBuffer(int bufferSize) {
+        this.bufferSize = bufferSize;
+        this.buffer = new String[bufferSize];
+    }
 
     public boolean isEmpty() {
-        int count = 0;
-        for (String str : this.buffer) {
-            if (str == null)
-                continue;
-
-            if (!str.isEmpty()) {
-                count++;
-            }
-        }
-        return count == 0;
+        return readPointer == writePointer;
     }
 
     public void writeData(String s) {
-        this.buffer[writePointer] = s;
-        writePointer++;
         writePointer %= bufferSize;
+        this.buffer[writePointer++] = s;
     }
 
-    public boolean isFUll() {
-        int count = 0;
-        for (String str : this.buffer) {
-            if (str == null)
-                continue;
-
-            if (!str.isEmpty()) {
-                count++;
-            }
-        }
-        return count == bufferSize;
+    public boolean isFull() {
+        return readPointer - writePointer == 1 || writePointer - readPointer == bufferSize;
     }
 
     public String readData() {
-        String result = this.buffer[readPointer];
-        this.buffer[readPointer] = "";
-        readPointer++;
         readPointer %= bufferSize;
-        return result;
+        return this.buffer[readPointer++];
     }
 }
